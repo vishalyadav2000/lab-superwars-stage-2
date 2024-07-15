021c5c14515c7c729edbf297134c02b4e830e20f
Vishal Yadav <144472297+vishalyadav2000@users.noreply.github.com>
2024-07-15 12:37:51 +0530

    app.js

--- src/app.js
+++ src/app.js
@@ -24,29 +24,42 @@ const PLAYERS = [
 // initialize players with image and strength
 const initPlayers = (players) => {
     let detailedPlayers = [];
-    // Create players using for loop
-    // Type your code here
-
+    for (let i = 0; i < players.length; i++) {
+        let player = {
+            name: players[i],
+            strength: getRandomStrength(),
+            image: "images/super-" + (i + 1) + ".png",
+            type: i % 2 === 0 ? 'hero' : 'villain'
+        };
+        detailedPlayers.push(player);
+    }
     return detailedPlayers;
 }
 
 // getting random strength
 const getRandomStrength = () => {
-    // Return a random integer (0,100]
-    // Note: You can use Math.random() and Math.ceil()
-}
+    return Math.ceil(Math.random() * 100);  
+};
 
 const buildPlayers = (players, type) => {
     let fragment = '';
-
-    // Loop through players and accumulate HTML template
-    // depending of type of player(hero|villain)
-    // Type your code here
-
+    for (let i = 0; i < players.length; i++) {
+        if (players[i].type === type) {
+            fragment += `
+                <div class="player">
+                    <img src="${players[i].image}" alt="">
+                    <div class="name">${players[i].name}</div>
+                    <div class="strength">${players[i].strength}</div>
+                </div>
+            `;
+        }
+    }
     return fragment;
 }
 // Display players in HTML
 const viewPlayers = (players) => {
+    const heroesHTML = buildPlayers(players, 'hero');
+    const villainsHTML = buildPlayers(players, 'villain');
 
     document.getElementById('heroes').innerHTML = buildPlayers(players, 'hero');
     document.getElementById('villains').innerHTML = buildPlayers(players, 'villain');
@@ -54,5 +67,6 @@ const viewPlayers = (players) => {
 }
 
 window.onload = () => {
+    const players = initPlayers(PLAYERS);
     viewPlayers(initPlayers(PLAYERS));
 }
